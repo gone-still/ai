5479f832d7cbf294317949e98bfaa5186d543497
gone-still <eldesgraciado@gmail.com>
2023-04-26 23:58:13 -0600

    Updated improvingAccuracy

--- machineLearning/onlineTests/improvingAccuracy/improvingAccuracy.py
+++ machineLearning/onlineTests/improvingAccuracy/improvingAccuracy.py
@@ -1,4 +1,4 @@
-# File        :   improvingAccuracy.py
+# File        :   testAnswer.py
 # Version     :   1.5.0
 # Description :   Solution to one of c0d1l1ty's Data Science "test"
 #
@@ -12,7 +12,6 @@
 # The data contains 7500 samples with 300 features. The feature
 # names are not important. There are no missing values.
 
-
 import numpy as np
 import pandas as pd
 
@@ -25,6 +24,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import cross_val_score
 
+from collections import Counter
+from sklearn.utils import resample
+
 
 class ClassifierWithImbalanceClass:
     def __init__(self):
@@ -34,7 +36,7 @@ class ClassifierWithImbalanceClass:
         # to the use of polynomial input variables.
         # StandardScaler to normalize features
         self._pipeline = make_pipeline(PolynomialFeatures(degree=2), StandardScaler(),
-                                       LinearSVC(C=1, random_state=42, dual=True, max_iter=10000))
+                                       LinearSVC(C=1, random_state=42, dual=False))
 
     def train(self, x, y):
         # Fit the pipeline:
@@ -46,10 +48,10 @@ class ClassifierWithImbalanceClass:
         classPredictions = self._pipeline.predict(x)
         return classPredictions
 
-    def getClassifier(self):
-        # Returns the classifier, so I can
+    def getPipeline(self):
+        # Returns the pipeline, so I can
         # run cross validation on it:
-        return self._pipeline[2]
+        return self._pipeline
 
 
 # Project Path:
@@ -59,32 +61,56 @@ projectPath = "D://dataSets//code//"
 trainDataset = "train_data.csv"
 testDataset = "test_data.csv"
 
+# Prediction label:
+predictionLabel = "classType"
+
 # Read the CSV Dataset:
 df_train = pd.read_csv(projectPath + trainDataset)
 y_train = df_train["target"]
 
-# Get correlation matrix:
-correlationMatrix = df_train.corr()
+# Check out the class distribution
+classLabels = df_train["target"]
+classCounter = Counter(classLabels)
 
-# Discard negative-correlated features:
-# Threshold:
-correlatedThreshold = 0.0
-# Get the candidate features that correlate to the target feature:
-candidateFeatures = correlationMatrix["target"]
+# Print the class distribution:
+for c in classCounter:
+    value = classCounter[c]
+    print("[Unbalanced] Class: ", c, " Count: ", value)
 
-# Apply the filter:
-importantFeatures = candidateFeatures[candidateFeatures >= correlatedThreshold]
-importantFeatures = importantFeatures.drop("target")
-# Get feature names as a list:
-importantFeatures = list(importantFeatures.index)
+# Set count of majority class:
+# Majority class is 0
+maxClassCount = int(classCounter[0] * 1.0)
 
-# Discard all features that are not important, keep only the
-# relevant features:
-X_train = df_train[importantFeatures]
+# Set the minority class to a separate dataframe:
+dfMinority = df_train[df_train["target"] == 1]
+# Set other classes to another dataframe:
+dfMajority = df_train[df_train["target"] != 1]
 
+# Up-sample the minority class:
+minorityUpscaled = resample(dfMinority, random_state=42, n_samples=maxClassCount, replace=True)
+
+# Concatenate the up-sampled dataframe. Now
+# the dataset should be balanced:
+df_train = pd.concat([minorityUpscaled, dfMajority])
+print(df_train.shape)
+
+# Check out the new class distribution
+classLabels = df_train["target"]
+classCounter = Counter(classLabels)
+
+# Print the class distribution:
+for c in classCounter:
+    value = classCounter[c]
+    print("[Balanced] Class: ", c, " Count: ", value)
+
+# Get training features and label:
+y_train = df_train["target"]
+X_train = df_train.drop("target", axis=1)
+
+# Read the test dataset:
 df_test = pd.read_csv(projectPath + testDataset)
 y_test = df_test["target"]
-X_test = df_test[importantFeatures]
+X_test = df_test.drop("target", axis=1)
 
 # Call and implement classifier:
 classifier = ClassifierWithImbalanceClass()
@@ -95,8 +121,13 @@ y_pred = classifier.predict(X_test)
 print(y_pred)
 print(type(y_pred))
 print(y_pred.shape)
+
 auc = metrics.roc_auc_score(y_test, y_pred)
-print(auc)
+print("ROC:", auc)
+
+# Get the pipeline:
+modelPipeline = classifier.getPipeline()
+print("Pipe Score:", modelPipeline.score(X_test, y_test))
 
 # Cross-validation folds:
 cvFolds = 5
@@ -104,15 +135,21 @@ cvFolds = 5
 parallelJobs = 5
 
 # Check out the reggressor accuracy using cross-validation:
-classifierAccuracy = cross_val_score(estimator=classifier.getClassifier(), X=X_train, y=y_train, cv=cvFolds,
-                                     n_jobs=parallelJobs,
-                                     verbose=3)
+classifierAccuracy = cross_val_score(estimator=modelPipeline, X=X_train, y=y_train, cv=cvFolds,
+                                     n_jobs=parallelJobs, verbose=3)
 
 # Accuracy for each fold:
-print(classifierAccuracy)
+print("Cross Score:", classifierAccuracy)
 # Mean accuracy and accuracy std dev:
 print("Accuracy Mean: ", np.mean(np.array(classifierAccuracy)), "Std Dev:", np.std(np.array(classifierAccuracy)))
 
 # Let's see the classification results:
-# for i in range(len(y_pred)):
-#     print(y_pred[i], y_test[i])
+for i in range(len(y_pred)):
+    realClass = y_test[i]
+    sampleClass = y_pred[i]
+
+    # Mark mismatches with an arrow:
+    missmatch = ""
+    if realClass != sampleClass:
+        missmatch = " <-"
+    print("Sample:", i, "Predicted:", sampleClass, "Truth:", realClass, "" + missmatch)
